16a1adc1c88cf717dd26aa1dfbd7adcb587f9511
Imaaneea <imaane.elayachi@gmail.com>
2025-09-22 12:52:33 +0100

    app.py

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ from tensorflow.keras import losses
 # Titre et logo
 # =========================
 st.set_page_config(page_title="Prévision des retraits GAB", layout="wide")
-st.image("https://www.albaridbank.ma/themes/baridbank/logo.png", use_column_width=True)
 st.title("Prévision des retraits GAB avec LSTM")
 st.write("Application basée sur le modèle LSTM pour prédire les retraits hebdomadaires des GAB.")
 
